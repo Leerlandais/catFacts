53c4ed597f55a8030a21f55cab3ced0e56e1fbe8
leerlandais <leerlandais@gmail.com>
2024-04-23 20:04:42 +0200

    did more and added comments

--- catfacts.js
+++ catfacts.js
@@ -1,21 +1,55 @@
+
+// AS USUAL, CREATE VARIABLES FOR THE HTML ITEMS
 const hideMe = document.getElementById("hideMe");
 const earlyFact = document.getElementById("earlyFact");
-const factList = document.getElementById("factList");
 const animalForm = document.getElementById("animalForm");
+let factList = document.getElementById("factList");
+
+getAnimalFacts(); // AS SOON AS THE PAGE LOADS, RUN THE FUNCTION AND DISPLAY A FACT
 
 animalForm.addEventListener('submit', function(event) { 
-    event.preventDefault();
-    getAnimalFacts();
+    event.preventDefault();         // DON'T ALLOW THE FORM TO SUBMIT
+    wipeAnimalFacts();              
 });
 
-
-function getAnimalFacts(animal = "cat", amount = 1) {
-    let animalType = document.querySelector('input[name="animalType"]:checked').value;
-    console.log(animalType)
-    $.get (`https://cat-fact.herokuapp.com/facts/random?animal_type=${animal}&amount=${amount}`, (fact) => {
-            earlyFact.textContent = fact.text;
-    })
+function wipeAnimalFacts () {           // DELETE EXISTING LIST-ITEMS (OTHERWISE THE LIST GROWS)
+    let deleteThis = factList.childElementCount;
+    while(deleteThis > 0) {
+        factList.removeChild(factList.firstElementChild);
+        deleteThis--;
+    }
+    factList = document.getElementById("factList");         // used Chat for coding for the first time in ages. Couldn't figure out what was going wrong with the deletion and addition of list-items (my code was fine but factList needed to be reassigned)
+    getAnimalFacts();
 }
 
 
-getAnimalFacts();
+function getAnimalFacts(animal = "cat", amount = 1) {       // DEFAULT SETTINGS IN CASE IT'S THE FIRST TIME THE PAGE LOADS
+
+    if (document.querySelector('input[name="animalType"]:checked')) {               // FIND WHICH RADIO INPUT IS CHECKED
+    animal = document.querySelector('input[name="animalType"]:checked').value;
+    }
+    amount = document.getElementById("animalAmount").value;
+    
+    amount < 2 ?                                                                                                    // TURNS OUT THAT TERNARIES CAN BE MORE THAN ONE LINE 
+    $.get (`https://cat-fact.herokuapp.com/facts/random?animal_type=${animal}&amount=${amount}`, (facts) => {
+            let fact = facts.text;
+        earlyFact.textContent = "a "+animal+" : "+fact;
+    }) 
+    : 
+    $.get (`https://cat-fact.herokuapp.com/facts/random?animal_type=${animal}&amount=${amount}`, (facts) => {
+        for (let fact of facts) {
+            thisFact = fact.text
+            makeAnimalList (thisFact);
+        }
+    })
+   
+}
+
+function makeAnimalList (fact) {                        // CREATE THE LIST-ITEMS, INSERT THE INFO AND CREATE THE LISTS
+    let LI = document.createElement("li");
+        LI.textContent = fact
+        factList.appendChild(LI);
+}
+
+
+
